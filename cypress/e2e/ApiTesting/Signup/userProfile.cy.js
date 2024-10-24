ea52122589fd0a543c6d2293684edf9c506968b0
kinzarao <kinza.tasleem@aldaimsolutions.com>
2024-10-24 18:01:16 +0500

    User Profile apis

--- cypress/e2e/ApiTesting/Signup/userProfile.cy.js
+++ cypress/e2e/ApiTesting/Signup/userProfile.cy.js
@@ -1,94 +1,98 @@
 
                                                       // Edit Profile BirthDay
    describe('User Profile', () => {
-      const authToken = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyODU1NiwiZXhwIjoxNzMwNTAwMzE0fQ.YiCZnHZM11o5hCugdgdVZhfApb8dtkzsVkA4JJA05sE'
-      const edenURL = 'https://8a94-2400-adc5-112-4b00-3e6f-52c8-2e52-58e3.ngrok-free.app/api/v1';
+      const authToken = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyODU4NywiZXhwIjoxNzMyMzk2MjI0fQ.R1KqrGfHyhrn7pcD1C2uLmMSZV5ly3i3ADow--oJiy0'
+      const edenURL = 'https://bluepalm.xyz/api/v1';
+      const phoneNumber = '+19876543210'
                                                     
-         it('Updates the user birth date', () => {
-             cy.request({
-             method: 'PUT',
-             url: `${edenURL}/profiles/`,  
-              headers: {
-                   "AUTH-TOKEN": authToken,
-                    'Content-Type': 'application/json'
+//          it('Updates the user birth date', () => {
+//              cy.request({
+//              method: 'PUT',
+//              url: `${edenURL}/profiles/`,  
+//               headers: {
+//                    "AUTH-TOKEN": authToken,
+//                     'Content-Type': 'application/json'
+// },
+//                body: {
+//                  "user": {
+//                  "birth_date": "2019-10-14 10:27:00"
+// }
+// }
+// }).then((response) => {
+//   expect(response.status).to.eq(201); 
+//   expect(response.body.data.attributes.phone).to.eq(phoneNumber);
+// });
+// })
+                                                    
+                                                    
+                                            //Edit userName
+it('Updates the user Name', () => {
+  cy.request({
+    method: 'PUT',
+    url: `${edenURL}/profiles/`,  
+  headers: {
+    "AUTH-TOKEN": authToken,
+    'Content-Type': 'application/json'
 },
-               body: {
-                 "user": {
-                 "birth_date": "2019-10-14 10:27:00"
+   body: {
+    "user": {
+    "user_name": "New testing"
+}
+  }
+}).then((response) => {
+   expect(response.status).to.eq(201); 
+   expect(response.body.data.attributes.phone).to.eq(phoneNumber);
+   expect(response.body.data.attributes.user_type).to.eq("external");
+});
+})
+
+                                                    // Edit InviteCode
+                                         
+it('Updates the user invitecode', () => {
+cy.request({
+   method: 'PUT',
+   url: `${edenURL}/profiles/`,  
+   headers: {
+       "AUTH-TOKEN": authToken,  // Replace with actual token if needed
+       'Content-Type': 'application/json'
+},
+   body: {
+       "user": {
+       "registration_code": "J689VN$"
 }
 }
 }).then((response) => {
   expect(response.status).to.eq(201); 
-  expect(response.body.data.attributes.phone).to.eq("+19876543210");
 });
 })
-                                                    
-                                                    
-                                                // Edit Profile UserName
-                                                                                                     
-                                                         it('Updates the user Name', () => {
-                                                            cy.request({
-                                                              method: 'PUT',
-                                                              url: `${edenURL}/profiles/`,  
-                                                            headers: {
-                                                              "AUTH-TOKEN": authToken,
-                                                              'Content-Type': 'application/json'
-                                                          },
-                                                             body: {
-                                                              "user": {
-                                                              "user_name": "New testing"
-                                                         }
-                                                            }
-                                                         }).then((response) => {
-                                                             expect(response.status).to.eq(201); 
-                                                         });
-                                                         })
-                                                    
-                                                        // Edit InviteCode
-                                                                                                   
-                                                        it('Updates the user invitecode', () => {
-                                                          cy.request({
-                                                             method: 'PUT',
-                                                             url: `${edenURL}/profiles/`,  
-                                                             headers: {
-                                                                 "AUTH-TOKEN": authToken,  // Replace with actual token if needed
-                                                                 'Content-Type': 'application/json'
-                                                        },
-                                                             body: {
-                                                                 "user": {
-                                                                 "registration_code": "J689VN$"
-                                                     }
-                                                         }
-                                                         }).then((response) => {
-                                                            expect(response.status).to.eq(201); 
-                                                    });
-                                                    })
-                                                                     
+           
                                                      //UnRead Item                                                 
-                                                        it('GET unread item', () => {
-                                                          cy.request({
-                                                             method: 'GET',
-                                                             url: `${edenURL}/profiles/unread_items`,  
-                                                             headers: {
-                                                                 "AUTH-TOKEN": authToken,  
-                                                        },
-                                                         }).then((response) => {
-                                                            expect(response.status).to.eq(200); 
-                                                    });
-                                                    })
-                                                    
-                                                        //Signed Url                                                 
-                                                        it('GET signed url', () => {
-                                                          cy.request({
-                                                             method: 'GET',
-                                                             url: `${edenURL}/profiles/edit?file_type=mov`,  
-                                                             headers: {
-                                                                 "AUTH-TOKEN": authToken,  
-                                                        },
-                                                         }).then((response) => {
-                                                            expect(response.status).to.eq(200); 
-                                                    });
-                                                     })
+it('GET unread item', () => {
+cy.request({
+   method: 'GET',
+   url: `${edenURL}/profiles/unread_items`,  
+   headers: {
+       "AUTH-TOKEN": authToken,  
+},
+}).then((response) => {
+  expect(response.status).to.eq(200); 
+});
+})
+
+                                                                                                     
+                                                      //Signed URL                                                       
+                                                                                                       
+it('GET signed url', () => {
+  cy.request({
+     method: 'GET',
+     url: `${edenURL}/profiles/edit?file_type=mov`,  
+     headers: {
+         "AUTH-TOKEN": authToken,  
+},
+ }).then((response) => {
+    expect(response.status).to.eq(201); 
+});
+})
                                                     
                                                     
                                                      //Menue Video                                                 
@@ -123,12 +127,14 @@
    it('GET userprofile', () => {
          cy.request({
          method: 'GET',
-         url: `${edenURL}/profiles/28556`,  
+         url: `${edenURL}/profiles/28587`,  
            headers: {
                   "AUTH-TOKEN": authToken,  
   },
    }).then((response) => {
-         expect(response.status).to.eq(200);              
+         expect(response.status).to.eq(200);
+         expect(response.body.data.id).to.eq("28587");   
+         expect(response.body.data.attributes.handle).to.eq("New testing");           
 });
 })
-})                                                    
+ })                                                    
